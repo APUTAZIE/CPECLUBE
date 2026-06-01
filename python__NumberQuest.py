ee8ee82ad08a193c5be4a43b5b50d88aa9d775e6
APUTAZIE <maryaputazie@gmail.com>
2026-06-01 21:35:39 +0100

    first version of number guessing game
    this project allows the user to guess a secret number within limited attempts. It gives hints like "too high" or "too low".

---
+++ python__NumberQuest.py
@@ -0,0 +1,25 @@
+ 
+secret_number = 13
+guess = 0
+attempts = 0
+
+while guess != secret_number and attempts < 3:
+
+    guess = int(input("Guess a number between 1 and 20: "))
+    attempts += 1
+
+    if guess  > secret_number:
+        print("Too high! Try again.")
+
+    elif guess < secret_number:
+        print("Too low! Try agan")
+
+    else:
+        print("Congratulations! You guessed the number!")
+        break
+if guess != secret_number:
+        print("Game Over! You have used all 3 attempts. ")
+
+        play_again = input("do you want to play again? (yes/no:) ").lower()
+        print("Thanks for playing Mind Teaser!")
+    
